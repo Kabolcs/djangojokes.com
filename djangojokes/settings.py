dec526b0dc8b83455fb93c5c042275b1b3b7c4e7
Kabolcs <b.szabbi@gmail.com>
2025-05-13 17:59:39 -0400

    email

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     #local apps
     'common.apps.CommonConfig',
+    'jobs.apps.JobsConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
