385e3aea1896e5cec60a4939b19945d7ccc0048a
Kabolcs <b.szabbi@gmail.com>
2025-06-16 12:35:00 -0400

    Media

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -180,6 +180,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # BOTTOM OF settings.py
 if os.environ.get('ENVIRONMENT') != 'production':
