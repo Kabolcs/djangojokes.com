4edffc008ef445fbd7af8a62dfc7f85a3918f337
Kabolcs <b.szabbi@gmail.com>
2025-06-17 11:18:31 -0400

    vote adj

--- jokes/models.py
+++ jokes/models.py
@@ -53,26 +53,6 @@ class Category(models.Model):
     def __str__(self):
         return self.category
     
-    class JokeVote(models.Model):
-        user = models.ForeignKey(
-            settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-            related_name='jokevotes'
-    )
-        joke = models.ForeignKey(
-            Joke, on_delete=models.CASCADE,
-            related_name='jokevotes'
-    )
-        vote = models.SmallIntegerField()
-        created = models.DateTimeField(auto_now_add=True)
-        updated = models.DateTimeField(auto_now=True)
-
-        class Meta:
-            constraints = [
-                models.UniqueConstraint(
-                    fields=['user', 'joke'], name='one_vote_per_user_per_joke'
-            )
-        ]
-    
 class Tag(models.Model):
     tag = models.CharField(max_length=50)
     slug = models.SlugField(
@@ -95,3 +75,24 @@ class Tag(models.Model):
     
     def __str__(self):
             return self.tag
+
+
+class JokeVote(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    joke = models.ForeignKey(
+        Joke, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    vote = models.SmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'joke'], name='one_vote_per_user_per_joke'
+            )
+        ]
